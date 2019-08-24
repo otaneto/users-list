66efeb7447f5534c5fa2f76c81b55f3860d45195
Otacílio Neto <otaneto@rogalabs.com>
2019-08-24 12:36:34 -0300

    added Topbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app column">
+    <topbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Topbar from '@/components/Topbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Topbar,
+  },
+}
+</script>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="topbar flex primary-darker">
+    <img width="200" src="@/assets/instruct.svg" alt="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar',
+}
+</script>
+
+<style>
+  .topbar {
+    background: no-repeat border-box;
+    padding: 20px 2% 20px 5%;
+  }
+</style>
