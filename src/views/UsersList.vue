a0e5e63d3574560efae9e328b72ff5066e4e51a7
Otacílio Neto <otaneto@rogalabs.com>
2019-08-24 12:48:48 -0300

    styling select

--- src/views/UsersList.vue
+++ src/views/UsersList.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container flex column align-center">
-    <select
-      v-model="filter"
-      class="filter"
-      name="filter"
-      placeholder="Filtrar"
-    >
-      <option value="*">Todos</option>
-      <template v-for="option in options">
-        <option :key="option" :value="option">{{ option }}</option>
-      </template>
-    </select>
+    <div class="flex align-center">
+      <span class="subtitle">Filtrar por domínio:&nbsp;&nbsp;</span>
+      <select
+        v-model="filter"
+        class="filter"
+        name="filter"
+        placeholder="Filtrar"
+      >
+        <option value="*">Todos</option>
+        <template v-for="option in options">
+          <option :key="option" :value="option">{{ option }}</option>
+        </template>
+      </select>
+    </div>
     <div class="container flex justify-space-between wrap">
       <template v-for="user in filteredUsers">
         <user-card
@@ -72,6 +75,45 @@ export default {
 
 <style>
   select {
-    width: 300px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: #1F3A54;
+    background-image: none;
+  }
+  .filter {
+    position: relative;
+    display: flex;
+    width: 20em;
+    height: 3em;
+    line-height: 3;
+    background: #1F3A54;
+    overflow: hidden;
+    border-radius: .25em;
+  }
+  select {
+    flex: 1;
+    padding: 0 .5em;
+    color: #fff;
+    cursor: pointer;
+    font-size: 0.9em;
+  }
+
+  .filter::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0 1em;
+    background: #1F3A54;
+    cursor: pointer;
+    pointer-events: none;
+    -webkit-transition: .25s all ease;
+    -o-transition: .25s all ease;
+    transition: .25s all ease;
   }
 </style>
