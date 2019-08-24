9414503a8a97e0a9a03eb6e32e614e2602597f9d
Otacílio Neto <otaneto@rogalabs.com>
2019-08-24 12:27:26 -0300

    changed main component to UsersList

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import UsersList from './views/UsersList.vue';
 
 Vue.use(Router);
 
@@ -10,8 +10,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      redirect: {
+        name: 'UsersList',
+      },
+    },
+    {
+      path: '/users-list',
+      name: 'UsersList',
+      component: UsersList,
     },
   ],
 });
