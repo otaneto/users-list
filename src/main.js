0167ae5f181812546cfc5b98eb30858e85f719e8
Otacílio Neto <otaneto@rogalabs.com>
2019-08-24 11:49:10 -0300

    added font-awesome and changed bas-styles extension to scss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
-import './assets/styles/basic-styles.css';
+import './assets/styles/basic-styles.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
