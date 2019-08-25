2893450394f93713372ec9c9bde62d8edaaea0d1
Otacílio Neto <otaneto@rogalabs.com>
2019-08-25 14:57:27 -0300

    added support  to cors

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const cors = require('cors');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  next();
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
