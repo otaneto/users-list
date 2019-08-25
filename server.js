1539c396d5cbac8fd65828fa8b35867a296948ca
Otacílio Neto <otaneto@rogalabs.com>
2019-08-25 15:34:29 -0300

    fix to get routes working

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const cors = require('cors');
+const history = require('connect-history-api-fallback');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.use(cors());
+
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+})
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
